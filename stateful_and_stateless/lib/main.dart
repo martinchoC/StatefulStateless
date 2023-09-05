353ebbc61b942a7bb2b9fc456421f86f3aafa88f
Martin <martin.e.castro.88@gmail.com>
2023-09-05 12:54:52 -0300

    Initial commit

---
+++ stateful_and_stateless/lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:stateful_and_stateless/pages/counter/counter_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        //colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+        useMaterial3: true,
+      ),
+      home: const CounterPage(),
+    );
+  }
+}
---
+++ stateful_and_stateless/lib/pages/counter/counter_page.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class CounterPage extends StatefulWidget {
+  const CounterPage({super.key});
+
+  @override
+  State<CounterPage> createState() => _CounterPageState();
+}
+
+class _CounterPageState extends State<CounterPage> {
+  int counter = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(
+        child: Text('$counter',
+        style: const TextStyle(fontSize: 40),),
+      ),
+      floatingActionButton: FloatingActionButton(onPressed: (){
+        counter++;
+        setState(() {});
+      }, 
+      backgroundColor: Colors.blue,
+      ),
+    );
+  }
+}
