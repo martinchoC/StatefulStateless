1858840364c1f2f79953fa04b6ca96f0909a80a1
Martin <martin.e.castro.88@gmail.com>
2023-09-21 20:50:23 -0300

    Basic form for login

--- stateful_and_stateless/lib/pages/login/login_page.dart
+++ stateful_and_stateless/lib/pages/login/login_page.dart
@@ -8,8 +8,49 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  String _email = '';
+  String _password = '';
+
+  bool get _isValidEmail {
+    return _email.contains("@");
+  }
+
+  bool get _isValidPassword {
+    return !_password.contains(" ") && _password.length >= 5;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          padding: const EdgeInsets.all(20),
+          width: double.infinity,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text("Email: "),
+              TextField(
+                onChanged: (value) {
+                  _email = value;
+                  setState(() {});
+                },
+              ),
+              const SizedBox(height: 30),
+              const Text("Password: "),
+              TextField(onChanged: (value) {
+                _password = value;
+                setState(() {});
+              }),
+              const SizedBox(height: 50),
+              MaterialButton(
+                onPressed: _isValidEmail && _isValidPassword ? () {} : null,
+                child: Text("SEND"),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
