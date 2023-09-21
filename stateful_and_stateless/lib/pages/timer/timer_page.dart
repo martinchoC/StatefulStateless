558b6f9af62453cc9260e044f3bb4f31444a6aa8
Martin <martin.e.castro.88@gmail.com>
2023-09-21 20:25:40 -0300

    dispose and didUpdateWidget methods used

--- stateful_and_stateless/lib/pages/timer/timer_page.dart
+++ stateful_and_stateless/lib/pages/timer/timer_page.dart
@@ -1,21 +1,75 @@
 import 'package:flutter/material.dart';
 import 'dart:async';
 
-class TimerPage extends StatefulWidget {
-  const TimerPage({super.key});
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
 
   @override
-  State<TimerPage> createState() => _TimerPageState();
+  State<HomePage> createState() => _HomePageState();
 }
 
-class _TimerPageState extends State<TimerPage> {
+class _HomePageState extends State<HomePage> {
+  bool _showTimer = true;
+  Color _color = Colors.primaries.first;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            if (_showTimer)
+              TimerView(
+                color: _color,
+              ),
+            Expanded(
+                child: ListView.builder(
+              itemBuilder: (_, index) {
+                final color = Colors.primaries[index];
+                return GestureDetector(
+                  child: Container(
+                    color: color,
+                    height: 50,
+                  ),
+                  onTap: () {
+                    _color = color;
+                    setState(() {});
+                  },
+                );
+              },
+              itemCount: Colors.primaries.length,
+            ))
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          _showTimer = !_showTimer;
+          setState(() {});
+        },
+      ),
+    );
+  }
+}
+
+class TimerView extends StatefulWidget {
+  const TimerView({super.key, required this.color});
+  final Color color;
+
+  @override
+  State<TimerView> createState() => _TimerViewState();
+}
+
+class _TimerViewState extends State<TimerView> {
   int _time = 0;
+  Timer? _timer;
 
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
-    Timer.periodic(const Duration(seconds: 1), (timer) {
+    print("initState");
+    _timer = Timer.periodic(const Duration(seconds: 1), (_) {
       _time++;
       setState(() {});
       if (_time > 10) {
@@ -25,14 +79,27 @@ class _TimerPageState extends State<TimerPage> {
   }
 
   @override
+  void didUpdateWidget(covariant TimerView oldWidget) {
+    // TODO: implement didUpdateWidget
+    super.didUpdateWidget(oldWidget);
+    print("old: ${oldWidget.color}");
+    print("current: ${widget.color}");
+  }
+
+  @override
+  void dispose() {
+    print("Timer dispose");
+    _timer?.cancel();
+    // TODO: implement dispose
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text(
-          '$_time',
-          style: const TextStyle(fontSize: 50),
-        ),
-      ),
+    print("timer build");
+    return Text(
+      '$_time',
+      style: TextStyle(fontSize: 50, color: widget.color),
     );
   }
 }
